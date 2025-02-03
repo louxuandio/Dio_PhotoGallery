892d7338a3a22d76e9ee3383349c8ec51fe4bb00
Lou Xuan Dio <louxuan@bu.edu>
2025-02-03 02:28:07 -0500

    fail to upload images with R.drawable.image_name.

--- app/src/main/java/com/example/diosphotogallery/MainActivity.kt
+++ app/src/main/java/com/example/diosphotogallery/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.R
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,17 +21,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diosphotogallery.ui.theme.DiosPhotoGalleryTheme
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             DiosPhotoGalleryTheme {
                 Column (
@@ -46,12 +66,26 @@ class MainActivity : ComponentActivity() {
                             fontWeight = FontWeight.Bold))
                     HorizontalDivider(
                         modifier = Modifier
-                            .fillMaxWidth(),
+                            .fillMaxWidth()
+                            .padding(8.dp),
                         color = Color.Gray,
                         thickness = 1.dp)
 
-                    Row {
-                        /*TODO: photo row 1*/
+                    Row (
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ){
+                        /*
+                        Image(
+                            painter = painterResource(id = R.drawable.nanjing),
+                            contentDescription = "Nanjing Image",
+                            modifier = Modifier
+                                .size(300.dp)
+                                .padding(top = 8.dp)
+                                //.align(Alignment.TopStart)
+                        )
+                        //put two photos in a line
+                         */
+
                     }
                     Row {
                         /*TODO: photo row 2*/
---
+++ app/src/main/java/com/example/diosphotogallery/ui/theme/Dio_PhotoGallery_View.kt
@@ -0,0 +1,21 @@
+package com.example.diosphotogallery.ui.theme
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.diosphotogallery.R
+
+class Dio_PhotoGallery_View : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dio_photo_gallery_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
