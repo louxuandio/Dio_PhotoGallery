dbe5fa9dfeb562a16cc98ca8b21539ecf15a97d4
Lou Xuan Dio <louxuan@bu.edu>
2025-02-03 14:52:48 -0500

    upload all of the images and texts, add padding and alignments for them.

--- app/src/main/java/com/example/diosphotogallery/MainActivity.kt
+++ app/src/main/java/com/example/diosphotogallery/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.diosphotogallery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,27 +12,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.diosphotogallery.ui.theme.DiosPhotoGalleryTheme
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.ui.unit.dp
@@ -78,29 +66,124 @@ class MainActivity : ComponentActivity() {
                     Row (
                         horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ){
-
-                        Box(
-                            modifier = Modifier
-                                .wrapContentSize()
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.nanjing),
-                                contentDescription = "Food Image",
+                        Column {
+                            Box(
                                 modifier = Modifier
-                                    .size(300.dp)
-                                    .padding(top = 8.dp)
-                                    .align(Alignment.TopStart)
-                            )
-                            
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.nanjing),
+                                    contentDescription = "Nanjing Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("Nanjing 2024.1",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
                         }
-
-
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.newyork),
+                                    contentDescription = "New York Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("New York 2024.12",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
+                        }
                     }
                     Row {
-                        /*TODO: photo row 2*/
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.cds),
+                                    contentDescription = "CDS Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("BU CDS 2023.10",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
+                        }
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.bucenter),
+                                    contentDescription = "BU CENTER Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("BU Center 2024.5",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
+                        }
                     }
                     Row {
-                        /*TODO: photo row 3*/
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.tufts),
+                                    contentDescription = "Tufts Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("Tufts 2024.3",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
+                        }
+                        Column {
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentSize()
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.harvard),
+                                    contentDescription = "Harvard Image",
+                                    modifier = Modifier
+                                        .size(200.dp)
+                                        .padding(top = 8.dp)
+                                        .align(Alignment.TopStart)
+                                )
+                            }
+                            Text("Harvard 2024.2",
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .align(Alignment.CenterHorizontally))
+                        }
                     }
                 }
             }
